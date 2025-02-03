39a5cbae7b22f89c784a8b82dc526e2d6247ec9d
arista <atd-help@arista.com>
2025-02-03 09:03:50 +0000

    Python all simple programs

---
+++ Python/fundamentals/planets.py
@@ -0,0 +1,2 @@
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+print(planets[2])
---
+++ Python/fundamentals/strings.py
@@ -0,0 +1,3 @@
+statement_part_1 = 'Hello'
+statement_part_2 = 'World'
+print(f"{statement_part_1} {statement_part_2}")
